d92b120e974ba89a28eeb2a4ebc2bb3ac3118ccf
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2022-12-04 03:16:04 +0800

    refactor cli and helpers pkg

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"os"
 	"path/filepath"
 	"strconv"
 	"time"
@@ -28,4 +29,31 @@ func IsLicenseExist() ([]string, error) {
 func GetYear() string {
 	year := time.Now().Year()
 	return strconv.Itoa(year)
+}
+
+func DeleteExistingLicenseFiles(skipName *string, existingFiles *[]string) error  {
+	for _, lic := range *existingFiles {
+		if lic == *skipName {
+			continue
+		}
+		err := os.Remove(lic)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func CreateAndWriteLicense(fileName *string, content *string) error {
+	f, osErr := os.Create(*fileName)
+	if osErr != nil {
+		return osErr
+	}
+
+	_, writeErr := f.WriteString(*content)
+	if writeErr != nil {
+		return writeErr
+	}
+
+	return nil
 }
